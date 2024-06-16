0f68c880d2ddfb4581065003afe84634321355d0
ShG1211 <88976926+ShG1211@users.noreply.github.com>
2024-06-16 20:21:38 +0800

    course还原

--- SpringBootWork/src/main/java/cn/edu/zucc/controller/CourseController.java
+++ SpringBootWork/src/main/java/cn/edu/zucc/controller/CourseController.java
@@ -1,9 +1,13 @@
 package cn.edu.zucc.controller;
 
 import cn.edu.zucc.dto.CourseDetailRequest;
+import cn.edu.zucc.dto.CourseModifyRequest;
 import cn.edu.zucc.dto.CourseNameRequest;
-import cn.edu.zucc.entity.Course;
+import cn.edu.zucc.dto.CourseReq1;
+import cn.edu.zucc.entity.*;
+import cn.edu.zucc.service.ClassroomService;
 import cn.edu.zucc.service.CourseService;
+import cn.edu.zucc.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +19,31 @@ import java.util.List;
 @RequestMapping("/course")
 public class CourseController {
     private final CourseService courseService;
+    private final ClassroomService classroomService;
+    private final TeacherService teacherService;
 
     @Autowired
-    public CourseController(CourseService courseService) {
+    public CourseController(CourseService courseService, ClassroomService classroomService, TeacherService teacherService) {
         this.courseService = courseService;
+        this.classroomService = classroomService;
+        this.teacherService = teacherService;
     }
 
-    // TODO:添加课程
     @PostMapping("/add_course")
-    public ResponseEntity<?> addCourse(@RequestBody Course course) {
+    public ResponseEntity<?> addCourse(@RequestBody CourseModifyRequest courseModifyRequest) {
+        System.out.println(courseModifyRequest);
+        Teacher teacher = teacherService.getTeacherById(courseModifyRequest.getTeacherId());
+        Classroom classroom = classroomService.getClassroomById(courseModifyRequest.getClassroomId());
+
+        Course course = new Course();
+        course.setCourseName(courseModifyRequest.getName());
+        course.setCredit(courseModifyRequest.getCredit());
+        course.setClassroom(classroom);
+        course.setCourseTeacher(teacher);
+        course.setCourseCapacity(courseModifyRequest.getCapacity());
+        course.setCompulsory(courseModifyRequest.isCompulsory());
+        course.setCourseTime(courseModifyRequest.getCourseTime());
+
         System.out.println(course);
         Course newCourse = courseService.createCourse(course);
         if (newCourse == null) {
@@ -33,9 +53,9 @@ public class CourseController {
         }
     }
 
-    // TODO:单个页面进去后可以删除课程
     @PostMapping("/delete_course")
-    public ResponseEntity<?> deleteCourse(@RequestBody Long courseId) {
+    public ResponseEntity<?> deleteCourse(@RequestBody CourseDetailRequest courseDetailRequest) {
+        Long courseId = courseDetailRequest.getCourseId();
         System.out.println(courseId);
         courseService.deleteCourse(courseId);
         return ResponseEntity.ok("Delete course successfully");
@@ -58,9 +78,22 @@ public class CourseController {
         }
     }
 
-    // TODO:修改课程信息
     @PostMapping("/update_course")
-    public ResponseEntity<?> updateCourse(@RequestBody Course course) {
+    public ResponseEntity<?> updateCourse(@RequestBody CourseReq1 courseModifyRequest) {
+        System.out.println(courseModifyRequest);
+        Teacher teacher = teacherService.getTeacherById(courseModifyRequest.getTeacherId());
+        Classroom classroom = classroomService.getClassroomById(courseModifyRequest.getClassroomId());
+
+        Course course = new Course();
+        course.setCourseId(courseModifyRequest.getCourseId());
+        course.setCourseName(courseModifyRequest.getName());
+        course.setCredit(courseModifyRequest.getCredit());
+        course.setClassroom(classroom);
+        course.setCourseTeacher(teacher);
+        course.setCourseCapacity(courseModifyRequest.getCapacity());
+        course.setCompulsory(courseModifyRequest.isCompulsory());
+        course.setCourseTime(courseModifyRequest.getCourseTime());
+
         System.out.println(course);
         Course newCourse = courseService.updateCourse(course);
         if (newCourse == null) {
